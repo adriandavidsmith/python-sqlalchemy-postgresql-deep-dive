0de39917bccef2f38e39728781223c94f18f9b09
Adrian Smith <adrian.smith@jumo.world>
2022-10-03 22:37:54 +0200

    Basic Table Setup

--- src/config/database/create-simple-table.sql
+++ src/config/database/create-simple-table.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE IF NOT EXISTS example_db_schema.public."table_one"
+(
+    id              serial primary key,
+    property_one    varchar(255) NOT NULL,
+    property_two    varchar(255) NOT NULL,
+    property_three  varchar(255) NOT NULL,
+    property_four   varchar(255) NOT NULL,
+    property_five   varchar(255) NOT NULL,
+    property_six    varchar(255) NOT NULL,
+    property_seven  varchar(255) NOT NULL,
+    property_eight  varchar(255) NOT NULL
+);
